1dff1a15dd6807edeb2a534abed4fe33bd5fb1bf
Farooq Butt <frukbutt196@gmail.com>
2025-03-15 02:44:24 +1100

    Added files to commit

--- ecom/urls.py
+++ ecom/urls.py
@@ -7,7 +7,7 @@ from myapp.views import home  # Import the home view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),  # Map the root path to the home view
+    path('', home, name='homepage'),  # Map the root path to the home view
 ]
 
 if settings.DEBUG:
