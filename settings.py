a1debfe296ca83aff7b386f2458d2a1a7b7bef4d
Farooq Butt <frukbutt196@gmail.com>
2025-03-15 02:35:57 +1100

    Added files to commit

--- settings.py
+++ settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Add your custom apps here
+    'myapp', # Add your custom apps here
 ]
 
 # Middleware
